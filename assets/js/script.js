a5c37d7164f948af9d6467925beef65f0d25cdb8
Xavy-Romeo <79165884+Xavy-Romeo@users.noreply.github.com>
2021-03-24 18:58:01 -0700

    added fetch and refactored code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,37 @@
 var cityInput = '';
-var cityInputData = {};
 
-// var createResultElements = function() {
+var createResultElements = function() {
+    var displayResultsDiv = $('<div>');
+        displayResultsDiv.addClass('results-current-div');
+    var displayForecastDiv = $('<div>');
+        displayForecastDiv.addClass('results-forcast-div');
+    
+    $('#results-container').append(displayResultsDiv, displayForecastDiv);
 
-// };
+    var resultsTitleDiv = $('<div>').addClass('row results-title-div');
+    var currentWeatherDiv = $('<div>').addClass('current-weather-div');
+    $('.results-current-div').append(resultsTitleDiv, currentWeatherDiv);
+    
+    var resultsTitle = $('<p>').addClass('results-title');
+    var titleIcon = $('<img>').addClass('title-icon');
+    $('.results-title-div').append(resultsTitle, titleIcon);
+
+    var temperatureEl = $('<p>')
+        .addClass('weather')
+        .html('Temperature: <span id="temperature"></span>');
+    var humidityEl = $('<p>')
+        .addClass('weather')
+        .html('Humidity: <span id="humidity"></span>');
+    var windSpeedEl = $('<p>')
+        .addClass('weather')
+        .html('Wind Speed: <span id="wind-speed"></span>');
+    var uvIndexEl = $('<p>')
+        .addClass('weather')
+        .html('UV Index: <span id="uv-index"></span>');
+    $('.current-weather-div').append(temperatureEl, humidityEl, windSpeedEl, uvIndexEl);
+
+    // create elements for forecast
+};
 
 var search = function(event) {
     // prevent refresh
@@ -12,39 +40,64 @@ var search = function(event) {
     cityInput = $('#search-input').val();
     // clear input value
     $('#search-input').val('');
-
     fetchCityWeather();
 };
 
 var fetchCityWeather = function() {
-    var apiUrl = 'https://api.openweathermap.org/data/2.5/forecast?q=' + cityInput + '&appid=3a01189ad2669a4fe12bba52ee8f9ead';
+    if (cityInput === '') {
+        var apiUrl = 'https://api.openweathermap.org/data/2.5/forecast?q=paris&appid=3a01189ad2669a4fe12bba52ee8f9ead&units=imperial';
+    }
+    else {
+        var apiUrl = 'https://api.openweathermap.org/data/2.5/forecast?q=' + cityInput + '&appid=3a01189ad2669a4fe12bba52ee8f9ead&units=imperial';
+    }
+    
     fetch(apiUrl)
     .then(function(response) {
         return response.json();
     })
     .then(function(response){
-        cityInputData = response;
+        var cityInputData = response;
+        fetchUvIndex(cityInputData);
         displayResults(cityInputData);
     });
 };
 
-// var displayResults = function(cityData) {
+var fetchUvIndex = function(cityData) {
+    var uvIndexUrl = 'http://api.openweathermap.org/data/2.5/uvi?lat=' + cityData.city.coord.lat + '&lon=' + cityData.city.coord.lon + '&appid=3a01189ad2669a4fe12bba52ee8f9ead';
     
-//     console.log('js28', cityData);
-// };
-
-var defaultStartup = function() {
-    // createResultElements();
-
-    var defaultApiUrl = 'https://api.openweathermap.org/data/2.5/forecast?q=paris&appid=3a01189ad2669a4fe12bba52ee8f9ead';
-    fetch(defaultApiUrl)
-    .then(function(parisData) {
-        return parisData.json();
+    fetch(uvIndexUrl)
+    .then(function(response) {
+        return response.json();
     })
-    .then(function(parisData){
-        displayResults(parisData);
-    });
+    .then(function(response) {
+        uvIndex = response.value;
+        $('#uv-index').text(uvIndex);
+    });  
+}; 
+
+var displayResults = function(cityData) {
+ 
+    var dateData = cityData.list[0].dt_txt.split(' ');
+    var date = dateData[0].split('-')
+    var dateFormat = '(' + date[1] + '/' + date[2] + '/' + date[0] + ')';  
+   
+    var iconUrl = 'http://openweathermap.org/img/wn/' + cityData.list[0].weather[0].icon +'@2x.png';
+    $('.title-icon').attr('src', iconUrl);
+
+    $('.results-title').text(cityData.city.name + ' ' + dateFormat);
+   
+    $('#temperature').text(Math.round((cityData.list[0].main.temp)*10)/10 + ' \xB0F');
+    $('#humidity').text(cityData.list[0].main.humidity + '%');
+    $('#wind-speed').text(Math.round((cityData.list[0].wind.speed)*10)/10 + ' MPH');
+    // $('#uv-index').text(uvIndex);
+
+// add forecast after making elements in createElementDivs
 };
 
-defaultStartup();
+var startup = function() {
+    fetchCityWeather();
+    createResultElements();
+};
+
+startup();
 $('.form').on('submit', search);
