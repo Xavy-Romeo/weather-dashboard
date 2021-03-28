9092d3faec61686d8ac490ed395d329ca3486667
Xavy-Romeo <79165884+Xavy-Romeo@users.noreply.github.com>
2021-03-28 02:24:17 -0700

    added local storage to store and display history

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,40 @@
 var cityInput = '';
 var idArr = [1,2,3,4,5];
-var iArr = []
 
-// new API I am switching to
+var search = function(event) {
+    // prevent refresh
+    event.preventDefault();
+    // store user input value for city
+    cityInput = $('#search-input').val().toUpperCase();
+    // clear input value
+    $('#search-input').val('');
+    fetchCityCoordinates();
+};
+
+var fetchCityCoordinates = function() {
+    if (cityInput === '') {
+        var inputApiUrl = 'https://api.openweathermap.org/data/2.5/forecast?q=paris&appid=3a01189ad2669a4fe12bba52ee8f9ead&units=imperial';
+    }
+    else {
+        var inputApiUrl = 'https://api.openweathermap.org/data/2.5/forecast?q=' + cityInput + '&appid=3a01189ad2669a4fe12bba52ee8f9ead&units=imperial';
+    }
+       
+    fetch(inputApiUrl)
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(response){
+        var cityInputData = response;
+                
+        // pull lat and lon for new api
+        cityLat = cityInputData.city.coord.lat;
+        cityLon = cityInputData.city.coord.lon;
+        
+        fetchCityWeather(cityInputData, cityLat, cityLon);
+        fetchUvIndex(cityInputData);
+    });
+};
+
 var fetchCityWeather = function(cityInputData, cityLat, cityLon) {
     var weatherApiUrl = 'https://api.openweathermap.org/data/2.5/onecall?lat=' + cityLat + '&lon=' + cityLon + '&appid=3a01189ad2669a4fe12bba52ee8f9ead&units=imperial';
     fetch(weatherApiUrl)
@@ -11,8 +43,38 @@ var fetchCityWeather = function(cityInputData, cityLat, cityLon) {
     })
     .then(function(cityData){
         displayResults(cityData, cityInputData);
+        storeHistory(cityInputData);
     });
 };
+
+var fetchUvIndex = function(cityInputData) {
+    var uvIndexUrl = 'http://api.openweathermap.org/data/2.5/uvi?lat=' + cityInputData.city.coord.lat + '&lon=' + cityInputData.city.coord.lon + '&appid=3a01189ad2669a4fe12bba52ee8f9ead';
+    
+    fetch(uvIndexUrl)
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(response) {
+        uvIndex = Math.round((response.value)*10)/10;
+        $('#uv-index').text(uvIndex);
+
+        if (uvIndex <= 2.999) {
+            $('#uv-index').css('background-color', 'green');
+        }
+        else if (uvIndex <= 5.999) {
+            $('#uv-index').css('background-color', 'yellow');
+        }
+        else if (uvIndex <= 7.999) {
+            $('#uv-index').css('background-color', 'orange');
+        }
+        else if (uvIndex <= 10.999) {
+            $('#uv-index').css('background-color', 'red');
+        }
+        else {
+            $('#uv-index').css('background-color', 'purple');
+        }
+    });  
+}; 
 
 var createResultElements = function() {
     var displayResultsDiv = $('<div>');
@@ -50,61 +112,17 @@ var createResultElements = function() {
     $('#forecast-container').append(forecastTitleEl, forecastDiv);
 
     for(i=0; i<5; i++) {
-    var forecastEl = $('<div>')
-        .addClass('forecast')
-        .attr('id', 'forecast' + idArr[i])
-        .html('<p class="forecast-dates" id=date'+ idArr[i] +'></p><img id=forecast-icon'+ idArr[i] +'></img><p id=temp'+ idArr[i] +'></p><p id=humidity'+ idArr[i]+'></p>')
+        var forecastEl = $('<div>')
+            .addClass('forecast')
+            .attr('id', 'forecast' + idArr[i])
+            .html('<p class="forecast-dates" id=date'+ idArr[i] +'></p>'+
+                '<img id=forecast-icon'+ idArr[i] +'></img>'+
+                '<p id=temp'+ idArr[i] +'></p>'+
+                '<p id=humidity'+ idArr[i]+'></p>');
  
-    forecastDiv.append(forecastEl);
+        forecastDiv.append(forecastEl);
     }
 };
-
-var search = function(event) {
-    // prevent refresh
-    event.preventDefault();
-    // store user input value for city
-    cityInput = $('#search-input').val();
-    // clear input value
-    $('#search-input').val('');
-    fetchCityCoordinates();
-};
-
-var fetchCityCoordinates = function() {
-    if (cityInput === '') {
-        var inputApiUrl = 'https://api.openweathermap.org/data/2.5/forecast?q=paris&appid=3a01189ad2669a4fe12bba52ee8f9ead&units=imperial';
-    }
-    else {
-        var inputApiUrl = 'https://api.openweathermap.org/data/2.5/forecast?q=' + cityInput + '&appid=3a01189ad2669a4fe12bba52ee8f9ead&units=imperial';
-    }
-       
-    fetch(inputApiUrl)
-    .then(function(response) {
-        return response.json();
-    })
-    .then(function(response){
-        var cityInputData = response;
-                
-        // pull lat and lon for new api
-        cityLat = cityInputData.city.coord.lat;
-        cityLon = cityInputData.city.coord.lon;
-        
-        fetchCityWeather(cityInputData, cityLat, cityLon);
-        fetchUvIndex(cityInputData);
-    });
-};
-
-var fetchUvIndex = function(cityInputData) {
-    var uvIndexUrl = 'http://api.openweathermap.org/data/2.5/uvi?lat=' + cityInputData.city.coord.lat + '&lon=' + cityInputData.city.coord.lon + '&appid=3a01189ad2669a4fe12bba52ee8f9ead';
-    
-    fetch(uvIndexUrl)
-    .then(function(response) {
-        return response.json();
-    })
-    .then(function(response) {
-        uvIndex = Math.round((response.value)*10)/10;
-        $('#uv-index').text(uvIndex);
-    });  
-}; 
 
 var displayResults = function(cityData, cityInputData) {
     console.log('cityData', cityData);   
@@ -137,6 +155,60 @@ var displayResults = function(cityData, cityInputData) {
     }
 };
 
+var storeHistory = function(cityInputData) {
+    var cityName = cityInputData.city.name.toUpperCase();
+    var historyArr;
+
+    if (cityInput === ''){
+        return;
+    }
+    else if (!localStorage.getItem('city'))
+    {
+        console.log('test test test 169');
+        var newHistoryArr = [];
+        newHistoryArr.push(cityName);
+        var jsonNewHistory = JSON.stringify(newHistoryArr);
+        localStorage.setItem('city', jsonNewHistory);
+    }
+    else {
+        var historyArrString = localStorage.getItem('city')
+        var historyArr = JSON.parse(historyArrString);
+               
+        for( var i = 1; i < historyArr.length; i++){ 
+                                   
+            if ( historyArr[i] === cityInput) { 
+                historyArr.splice(i, 1); 
+            }
+        };
+
+        historyArr.unshift(cityName);
+        historyArr.splice(8);
+        var jsonHistory = JSON.stringify(historyArr);
+        localStorage.setItem('city', jsonHistory);
+    }
+   
+   displayHistory();
+};
+
+var displayHistory = function() {
+    $('.history-items').remove();
+    var historyArr = JSON.parse(localStorage.getItem('city'));
+    
+    if (!historyArr) {
+        return;
+    }
+    else {
+        for (i = 0; i < historyArr.length; i++) {
+            var historyDiv = $('<p>')
+                .addClass('history-items')
+                .text(historyArr[i]);
+        
+            $('#history-container').append(historyDiv);
+        }
+    }
+};
+
 fetchCityCoordinates();
 createResultElements();
+displayHistory();
 $('.form').on('submit', search);
