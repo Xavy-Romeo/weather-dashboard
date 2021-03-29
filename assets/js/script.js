0ec92621e8462f0e33717449358f17d0b1d1bdac
Xavy-Romeo <79165884+Xavy-Romeo@users.noreply.github.com>
2021-03-28 23:05:20 -0700

    added notes to code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+// stores user input value
 var cityInput = '';
 var idArr = [1,2,3,4,5];
 
@@ -12,13 +13,16 @@ var search = function(event) {
 };
 
 var fetchCityCoordinates = function() {
+    // run a default page load fetch
     if (cityInput === '') {
-        var inputApiUrl = 'https://api.openweathermap.org/data/2.5/forecast?q=paris&appid=3a01189ad2669a4fe12bba52ee8f9ead&units=imperial';
+        var inputApiUrl = 'https://api.openweathermap.org/data/2.5/forecast?q=torrance&appid=3a01189ad2669a4fe12bba52ee8f9ead&units=imperial';
     }
+    // fetch info based on user input
     else {
         var inputApiUrl = 'https://api.openweathermap.org/data/2.5/forecast?q=' + cityInput + '&appid=3a01189ad2669a4fe12bba52ee8f9ead&units=imperial';
     }
-       
+    
+    // fetch data
     fetch(inputApiUrl)
     .then(function(response) {
         return response.json();
@@ -31,12 +35,13 @@ var fetchCityCoordinates = function() {
         cityLon = cityInputData.city.coord.lon;
         
         fetchCityWeather(cityInputData, cityLat, cityLon);
-        fetchUvIndex(cityInputData);
+        fetchUvIndex(cityLat, cityLon);
     });
 };
 
 var fetchCityWeather = function(cityInputData, cityLat, cityLon) {
     var weatherApiUrl = 'https://api.openweathermap.org/data/2.5/onecall?lat=' + cityLat + '&lon=' + cityLon + '&appid=3a01189ad2669a4fe12bba52ee8f9ead&units=imperial';
+    // fetch weather data based on lat & lon
     fetch(weatherApiUrl)
     .then(function(cityData){
         return cityData.json();
@@ -47,9 +52,11 @@ var fetchCityWeather = function(cityInputData, cityLat, cityLon) {
     });
 };
 
-var fetchUvIndex = function(cityInputData) {
-    var uvIndexUrl = 'http://api.openweathermap.org/data/2.5/uvi?lat=' + cityInputData.city.coord.lat + '&lon=' + cityInputData.city.coord.lon + '&appid=3a01189ad2669a4fe12bba52ee8f9ead';
+var fetchUvIndex = function(cityLat, cityLon) {
+    // store value of url based on lat & lon
+    var uvIndexUrl = 'http://api.openweathermap.org/data/2.5/uvi?lat=' + cityLat + '&lon=' + cityLon + '&appid=3a01189ad2669a4fe12bba52ee8f9ead';
     
+    // fetch UV index data
     fetch(uvIndexUrl)
     .then(function(response) {
         return response.json();
@@ -58,6 +65,7 @@ var fetchUvIndex = function(cityInputData) {
         uvIndex = Math.round((response.value)*10)/10;
         $('#uv-index').text(uvIndex);
 
+        // Set color based on UV index
         if (uvIndex <= 2.999) {
             $('#uv-index').css('background-color', 'green');
         }
@@ -76,7 +84,9 @@ var fetchUvIndex = function(cityInputData) {
     });  
 }; 
 
+// creates page elements
 var createResultElements = function() {
+    // creates divs to hold current and forecast data
     var displayResultsDiv = $('<div>');
         displayResultsDiv.addClass('results-current-div');
     var displayForecastDiv = $('<div>');
@@ -84,6 +94,7 @@ var createResultElements = function() {
     
     $('#results-container').append(displayResultsDiv, displayForecastDiv);
 
+    // current weather elements
     var resultsTitleDiv = $('<div>').addClass('row results-title-div');
     var currentWeatherDiv = $('<div>').addClass('current-weather-div');
     displayResultsDiv.append(resultsTitleDiv, currentWeatherDiv);
@@ -105,12 +116,15 @@ var createResultElements = function() {
         .addClass('weather')
         .html('UV Index: <span id="uv-index"></span>');
     currentWeatherDiv.append(temperatureEl, humidityEl, windSpeedEl, uvIndexEl);
+    // end current weather elements
 
+    // forecast weather elements
     var forecastTitleEl = $('<p>').attr('id', 'forecast-title').text('5-Day Forecast:');
     var forecastDiv = $('<div>').addClass('row forecastDiv');
  
     $('#forecast-container').append(forecastTitleEl, forecastDiv);
 
+    // for loop for 5 day forecast 
     for(i=0; i<5; i++) {
         var forecastEl = $('<div>')
             .addClass('forecast')
@@ -122,46 +136,60 @@ var createResultElements = function() {
  
         forecastDiv.append(forecastEl);
     }
+    // end forecast elements
 };
 
 var displayResults = function(cityData, cityInputData) {
-    console.log('cityData', cityData);   
+    // get unix date data and format it
     var unixDateData = cityData.current.dt;
     var date = new Date(unixDateData*1000).toLocaleDateString('en-US');
     var dateFormat = '(' + date + ')';
     
+    // get weather icon based on weather data
     var iconUrl = 'http://openweathermap.org/img/wn/' + cityInputData.list[0].weather[0].icon +'@2x.png';
     $('.title-icon').attr('src', iconUrl);
 
+    // current weather data
+    // current weather title
     $('.results-title').text(cityInputData.city.name + ' ' + dateFormat);
-   
+    // current temperature
     $('#temperature').text(Math.round((cityData.current.temp)*10)/10 + ' \xB0F');
+    // current humidity
     $('#humidity').text(cityData.current.humidity + '%');
+    // current wind speed
     $('#wind-speed').text(Math.round((cityData.current.wind_speed)*10)/10 + ' MPH');
 
+    // for loop for forecast
     for (i = 0; i < 5; i++) {
+        // forecast date
         var unixDate = cityData.daily[i+1].dt*1000;
         var forecastDate = new Date (unixDate).toLocaleDateString('en-US'); 
         $('#date' + idArr[i]).text(forecastDate);
        
+        // forecast weather icon
         var forecastIconUrl = 'http://openweathermap.org/img/wn/' + cityData.daily[i].weather[0].icon +'@2x.png';
         $('#forecast-icon' + idArr[i]).attr('src', forecastIconUrl);
         
+        // forecast temperature
         var forecastTemp = (cityData.daily[i].temp.max).toFixed(1);
         $('#temp' + idArr[i]).text('Temp: ' + forecastTemp + ' \xB0F');
-
+        
+        // forecast humidity
         var forecastHumidity = cityData.daily[i].humidity;
         $('#humidity' + idArr[i]).text('Humidity: ' + forecastHumidity + ' \xB0F');
     }
 };
 
+// function to store history of user inputs
 var storeHistory = function(cityInputData) {
     var cityName = cityInputData.city.name.toUpperCase();
     var historyArr;
 
+    // do not save default search
     if (cityInput === ''){
         return;
     }
+    // create local storage object
     else if (!localStorage.getItem('city'))
     {
         console.log('test test test 169');
@@ -170,18 +198,22 @@ var storeHistory = function(cityInputData) {
         var jsonNewHistory = JSON.stringify(newHistoryArr);
         localStorage.setItem('city', jsonNewHistory);
     }
+    // retrieve local storage info and add each additional input
     else {
         var historyArrString = localStorage.getItem('city')
         var historyArr = JSON.parse(historyArrString);
                
         for( var i = 1; i < historyArr.length; i++){ 
-                                   
+            // do not show duplicate values in search history                       
             if ( historyArr[i] === cityInput) { 
                 historyArr.splice(i, 1); 
             }
         };
 
+        // add each user input to local storage
+        // place most recent user input search item at the top
         historyArr.unshift(cityName);
+        // set maximum history to 8 inputs
         historyArr.splice(8);
         var jsonHistory = JSON.stringify(historyArr);
         localStorage.setItem('city', jsonHistory);
@@ -190,14 +222,18 @@ var storeHistory = function(cityInputData) {
    displayHistory();
 };
 
+// function to display user search history 
 var displayHistory = function() {
+    // clear previous history to not display duplicate histories
     $('.history-items').remove();
     var historyArr = JSON.parse(localStorage.getItem('city'));
     
+
     if (!historyArr) {
         return;
     }
     else {
+        // loop to create a <p> element for each search item in history
         for (i = 0; i < historyArr.length; i++) {
             var historyDiv = $('<p>')
                 .addClass('history-items')
@@ -208,7 +244,10 @@ var displayHistory = function() {
     }
 };
 
+// run programs on start-up
 fetchCityCoordinates();
 createResultElements();
 displayHistory();
+
+// start search on user submit
 $('.form').on('submit', search);
